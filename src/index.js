e893aef63b8ac237f706c6ab141e3050c7729231
pryvvid <pryvvid@gmail.com>
2019-03-18 19:20:53 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsType = [];
+  let temp = str;
+  let counter = Math.round(str.length / 2);
+
+  for (let i of bracketsConfig) {
+    bracketsType = [...bracketsType, i[0]+i[1]];
+  }
+
+  while (true) {
+    counter--;
+    for (let i of bracketsType) {
+      if (temp.indexOf(i) != -1 && typeof temp === 'string') temp = temp.split(i);
+      if (typeof temp === 'object') temp = temp.join('');
+      if (temp.length === 0) return true;
+      if (counter < 1) return false;
+    }
+  }
 }
